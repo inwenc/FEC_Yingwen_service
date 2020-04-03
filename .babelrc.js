73492a7da3d26133872500eab3ccb5afed84dd41
Yingwen <inwen.chen@gmail.com>
2020-04-03 09:18:00 -0600

    changing color to onFocus on checkin and checkout buttons

---
+++ .babelrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+//   env: {
+//     test: {
+//       plugins: ["@babel/plugin-transform-modules-commonjs"]
+//     }
+// }
+env: {
+  test: {
+    presets: ["@babel/preset-env", "@babel/preset-react"],
+    plugins: ["transform-export-extensions", "@babel/plugin-transform-modules-commonjs"],
+    only: [
+      "./**/*.js",
+      "node_modules/jest-runtime",
+      "../src"
+    ]
+  }
+}
+}
---
+++ jest.config 2.js
@@ -0,0 +1,9 @@
+module.exports = {
+ // transformIgnorePatterns: [`/node_modules/(?!${esModules})`]
+ testEnvironment: 'node',
+ coveragePathIgnorePatterns: [
+  "/node_modules/"
+],
+moduleFileExtensions: ["js", "json", "jsx", 'ts', 'tsx']
+
+}
